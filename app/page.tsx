cf18dc3b1ce9aa1504dffc462f476413282474c1
Jitendra Kumawat <jitendrak812000@gmail.com>
2023-07-19 14:30:47 +0530

    add pagination, sorting

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { createContext, useContext, useEffect, useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
 import DataTable from "../components/DataTable";
 
+const DataContext = createContext<any>(null);
+
 const Home: React.FC = () => {
+  const [rowsPerPage, setRowsPerPage] = useState<number>(10);
   const [data, setData] = useState<any>([]);
+  const [page, setPage] = useState<any>([]);
+  const [sortColumn, setSortColumn] = useState<string | null>(null);
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+  const [pageNumber, setPageNumber] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,8 +21,9 @@ const Home: React.FC = () => {
         const response = await fetch(
           "https://my.api.mockaroo.com/data.json?key=94929a20"
         );
-        const data = await response.json();
-        setData(data);
+        const tempData: any[] = await response.json();
+        setData(tempData);
+        setTotalPages(Math.ceil(tempData.length / rowsPerPage));
       } catch (err) {
         console.log(err);
       }
@@ -23,19 +32,73 @@ const Home: React.FC = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (sortColumn) {
+      console.log(sortColumn);
+
+      const sortedData = data.sort((a: any, b: any) => {
+        if (sortColumn === "timestamp") {
+          const aArray = a[sortColumn].split(" ");
+          const bArray = b[sortColumn].split(" ");
+          const aMinutes =
+            aArray[1] === "minutes"
+              ? parseInt(aArray[0])
+              : parseInt(aArray[0]) * 60;
+          const bMinutes =
+            bArray[1] === "minutes"
+              ? parseInt(bArray[0])
+              : parseInt(bArray[0]) * 60;
+
+          return sortDirection === "asc"
+            ? aMinutes - bMinutes
+            : bMinutes - aMinutes;
+        } else {
+          return sortDirection === "asc"
+            ? a[sortColumn].localeCompare(b[sortColumn])
+            : b[sortColumn].localeCompare(a[sortColumn]);
+        }
+      });
+      setData(sortedData);
+    }
+    const currentPage = data.slice(
+      pageNumber * rowsPerPage,
+      pageNumber * rowsPerPage + rowsPerPage
+    );
+    setPage(currentPage);
+  }, [data, sortColumn, sortDirection, pageNumber, rowsPerPage]);
+
   return (
-    <ChakraProvider>
-      {data.length != 0 && (
-        <DataTable
-          caption="Bookings"
-          headers={Object.keys(data[0])}
-          rows={data}
-          sortable
-          pagination
-        />
-      )}
-    </ChakraProvider>
+    <DataContext.Provider
+      value={{
+        rowsPerPage,
+        setRowsPerPage,
+        sortColumn,
+        setSortColumn,
+        sortDirection,
+        setSortDirection,
+        pageNumber,
+        setPageNumber,
+        totalPages,
+      }}
+    >
+      <ChakraProvider>
+        {data.length !== 0 && (
+          <div className="table-container">
+            <DataTable
+              caption="Bookings"
+              headers={Object.keys(data[0])}
+              page={page}
+              sortable
+              pagination
+            />
+          </div>
+        )}
+      </ChakraProvider>
+    </DataContext.Provider>
   );
 };
 
+const useDataContext = () => useContext(DataContext);
+
 export default Home;
+export { DataContext, useDataContext };
--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   Box,
   Table,
@@ -11,6 +9,9 @@ import {
   Tr,
   Flex,
   IconButton,
+  Input,
+  Select,
+  Badge,
 } from "@chakra-ui/react";
 import {
   ArrowRightIcon,
@@ -20,11 +21,12 @@ import {
 } from "@chakra-ui/icons";
 import { useState } from "react";
 import "../app/globals.css";
+import { DataContext, useDataContext } from "@/app/page";
 
 interface DataTableProps {
   caption?: string;
   headers: string[];
-  rows: (string | JSX.Element)[][];
+  page: (string | JSX.Element)[][];
   sortable?: boolean;
   pagination?: boolean;
 }
@@ -32,36 +34,76 @@ interface DataTableProps {
 const DataTable: React.FC<DataTableProps> = ({
   caption,
   headers,
-  rows,
+  page,
   sortable,
   pagination,
 }) => {
-  const [sortedColumn, setSortedColumn] = useState<string | null>(null);
-  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 10;
+  const dataContext = useDataContext();
+
+  const {
+    sortColumn,
+    setSortColumn,
+    setSortDirection,
+    sortDirection,
+    pageNumber,
+    setPageNumber,
+    rowsPerPage,
+    setRowsPerPage,
+    totalPages,
+  } = dataContext;
 
   const handleHeaderClick = (column: string) => {
     if (sortable) {
-      if (sortedColumn === column) {
+      if (sortColumn === column) {
         setSortDirection(sortDirection === "asc" ? "desc" : "asc");
       } else {
-        setSortedColumn(column);
+        setSortColumn(column);
         setSortDirection("asc");
       }
     }
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+
+    setInputValue(value);
+  };
+
+  const handleInputKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      inputValue < 1
+        ? setPageNumber(0)
+        : inputValue > totalPages
+        ? setPageNumber(totalPages - 1)
+        : setPageNumber(inputValue - 1);
+    }
+  };
+
   const handleNextPage = () => {
-    setCurrentPage(currentPage + 1);
+    setPageNumber((prev: number) => (prev += 1));
   };
 
   const handlePrevPage = () => {
-    setCurrentPage(currentPage - 1);
+    setPageNumber((prev: number) => (prev -= 1));
   };
 
+  const handleFirstPage = () => {
+    setPageNumber(0);
+  };
+
+  const handleLastPage = () => {
+    setPageNumber(totalPages - 1);
+  };
+
+  const [inputValue, setInputValue] = useState<number>(pageNumber + 1);
+
   return (
-    <Box className="data-table">
+    <Box
+      className="data-table"
+      height="100%"
+      display="flex"
+      flexDirection="column"
+    >
       <Text className="table-heading">DataTable</Text>
       <Table className="table" variant="striped">
         <Thead>
@@ -78,7 +120,7 @@ const DataTable: React.FC<DataTableProps> = ({
           </Tr>
         </Thead>
         <Tbody>
-          {rows.map((row, index) => (
+          {page.map((row, index) => (
             <Tr key={index}>
               {Object.values(row).map((cell, cellIndex) => (
                 <Td key={cellIndex}>{cell}</Td>
@@ -98,24 +140,53 @@ const DataTable: React.FC<DataTableProps> = ({
               className="pagination-btns"
               aria-label="first-page"
               icon={<ArrowLeftIcon h={3} w={3} />}
-            ></IconButton>
+              onClick={handleFirstPage}
+            />
             <IconButton
               className="pagination-btns"
               aria-label="previous-page"
               icon={<ChevronLeftIcon h={6} w={6} />}
-            ></IconButton>
+              onClick={handlePrevPage}
+            />
+          </Flex>
+          <Flex alignItems="center">
+            <Flex justifyContent="space-between" alignItems="center" mx={4}>
+              <Text mx={2} whiteSpace="nowrap">
+                Jump to Page :
+              </Text>
+              <Input
+                value={inputValue}
+                type="number"
+                onChange={handleInputChange}
+                onKeyPress={handleInputKeyPress}
+                width={20}
+              />
+            </Flex>
+            <Flex>
+              <Badge p={2} px={4}>
+                {`Page ${pageNumber + 1} of ${totalPages}`}
+              </Badge>
+            </Flex>
+            <Flex justifyContent="space-between" alignItems="center" mx={4}>
+              <Text mx={2} whiteSpace="nowrap">
+                Rows per Page :{" "}
+              </Text>
+              <Select />
+            </Flex>
           </Flex>
           <Flex className="pagination-btn-group">
             <IconButton
               className="pagination-btns"
-              aria-label="last-page"
-              icon={<ArrowRightIcon h={3} w={3} />}
-            ></IconButton>
-            <IconButton
-              className="pagination-btns"
               aria-label="next-page"
               icon={<ChevronRightIcon h={6} w={6} />}
-            ></IconButton>
+              onClick={handleNextPage}
+            />
+            <IconButton
+              className="pagination-btns"
+              aria-label="last-page"
+              icon={<ArrowRightIcon h={3} w={3} />}
+              onClick={handleLastPage}
+            />
           </Flex>
         </Flex>
       )}
